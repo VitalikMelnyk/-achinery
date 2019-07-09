55d6c56661b0444de8acd58ac400fadd217ae94d
unknown <melnyk.vitalikk@gmail.com>
2019-07-09 21:20:40 +0300

    some work with slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    //   $('[data-slider]').slick({
+    //     infinity: true,
+    //     fade: false,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    // })
+
+
+
+})
