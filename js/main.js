0a0ef4b9fc124f8e64fa3bab64ba8b609d0de486
unknown <melnyk.vitalikk@gmail.com>
2019-07-16 00:25:16 +0300

    fixed scroll nav

--- js/main.js
+++ js/main.js
@@ -1,13 +1,47 @@
 $(function() {
-    //   $('[data-slider]').slick({
-    //     infinity: true,
-    //     fade: false,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1
-    // })
-    
+    // Declaration
+
+    var headerHeight = $('.header-information').innerHeight(),
+        navigation = $('#nav'),
+        scrollOffSet = $(window).scrollTop();  
+        checkScroll(scrollOffSet);      
+
+
+
+    // Fixed navigation
+    $(window).on('scroll', function() {
+        scrollOffSet = $(this).scrollTop();
+        checkScroll(scrollOffSet);  
+    })
+
+    function checkScroll(scrollOffSet) {
+        if(scrollOffSet >= headerHeight) {
+            navigation.addClass('fixed');
+        }
+        else {
+            navigation.removeClass('fixed');
+        }
+    }
+    // ------------------- //
+
+
+
+    // Add active class on navigation-link
+
+
+
+
+    // ------------------- //
+
+
+    // Burger menu
     $('#nav-toggle').on('click', function(event) {
         $('.header-nav').toggleClass('active');
     })
+    // ------------------- //
+
+
+
+
 
 })
